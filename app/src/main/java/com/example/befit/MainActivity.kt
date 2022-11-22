1f4434407e7b3100021fac69ea5aea057c831c12
Jakub Syska <jsyska@it-dev.pl>
2022-11-22 01:26:01 +0100

    Added date picker and fixed dark theme

--- app/src/main/java/com/example/befit/MainActivity.kt
+++ app/src/main/java/com/example/befit/MainActivity.kt
@@ -4,15 +4,31 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
+import com.example.befit.databinding.ActivityMainBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding;
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         val splashScreen = installSplashScreen()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
         WindowCompat.setDecorFitsSystemWindows(window, false)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+        val datePicker =
+            MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select date")
+                .build()
+
+        binding.calendar.setOnClickListener {
+            datePicker.show(supportFragmentManager, "tag")
+        }
     }
 }
