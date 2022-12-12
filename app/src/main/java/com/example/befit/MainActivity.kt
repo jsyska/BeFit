34017550b471bfb9b57d13e4714fda93794daf89
Jakub Syska <jsyska@it-dev.pl>
2022-12-12 22:43:00 +0100

    Added select quantity dialog

--- app/src/main/java/com/example/befit/MainActivity.kt
+++ app/src/main/java/com/example/befit/MainActivity.kt
@@ -5,15 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import com.example.befit.databinding.ActivityMainBinding
 import com.example.befit.databinding.DialogAddProductBinding
+import com.example.befit.databinding.DialogSelectQuantityBinding
 import com.example.befit.services.FoodApi
 import com.example.befit.services.FoodApiResponse
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -23,6 +27,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var dialogBinding: DialogAddProductBinding
+    private lateinit var quantityDialogBinding: DialogSelectQuantityBinding
     private val dateFormat = SimpleDateFormat("dd/MM/yy")
     private val dayFormat = SimpleDateFormat("EEEE")
 
@@ -57,6 +62,40 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.copyButton.setOnClickListener{
+            FoodApi.retrofitService.getProperties("80177173").enqueue(object:
+                Callback<FoodApiResponse> {
+                override fun onFailure(call: Call<FoodApiResponse>, t: Throwable) {
+                    var _response = t.message
+                }
+
+                override fun onResponse(
+                    call: Call<FoodApiResponse>,
+                    response: Response<FoodApiResponse>
+                ) {
+                    var _response: Response<FoodApiResponse> = response
+                    if(_response.isSuccessful){
+
+                        if(_response.body()?.Status == 1){
+                            val quantityDialog = MaterialAlertDialogBuilder(binding.copyButton.context)
+                            quantityDialog.setTitle(_response.body()?.Product?.ProductName)
+                            val view = layoutInflater.inflate(R.layout.dialog_select_quantity, null);
+                            quantityDialogBinding = DialogSelectQuantityBinding.inflate(LayoutInflater.from(binding.copyButton.context))
+                            quantityDialog.setView(quantityDialogBinding.root)
+                            quantityDialog.setPositiveButton("Ok", null)
+                            quantityDialog.setNegativeButton("Cancel", null);
+                            quantityDialog.show()
+                        } else{
+                            Toast.makeText(binding.copyButton.context, "No product was found in database.", Toast.LENGTH_SHORT)
+                        }
+                    } else {
+                        Toast.makeText(binding.copyButton.context, "Something went wrong while making a request.", Toast.LENGTH_SHORT)
+                    }
+
+                }
+            })
+        }
+
         binding.addButton.setOnClickListener {
             val dialog = MaterialAlertDialogBuilder(this)
             dialog.setTitle("Add product")
@@ -75,8 +114,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private val getContent =
-        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+    private val getContent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             if (it.resultCode == Activity.RESULT_OK) {
                 val barcode = it?.data?.getStringExtra("BarcodeResult")
                 if (barcode != null) {
@@ -84,13 +122,35 @@ class MainActivity : AppCompatActivity() {
                         Callback<FoodApiResponse> {
                         override fun onFailure(call: Call<FoodApiResponse>, t: Throwable) {
                             var _response = t.message
+                            Toast.makeText(dialogBinding.scan.context, "Unable to get product data.", Toast.LENGTH_SHORT)
                         }
 
                         override fun onResponse(
                             call: Call<FoodApiResponse>,
                             response: Response<FoodApiResponse>
                         ) {
-                            var _response = response
+                            var _response: Response<FoodApiResponse> = response
+
+                            if(_response.isSuccessful){
+
+                                if(_response.body()?.Status == 1){
+
+                                    val quantityDialog = MaterialAlertDialogBuilder(dialogBinding.scan.context)
+                                    quantityDialog.setTitle(_response.body()?.Product?.ProductName)
+                                    val view = layoutInflater.inflate(R.layout.dialog_select_quantity, null);
+                                    quantityDialogBinding = DialogSelectQuantityBinding.inflate(LayoutInflater.from(dialogBinding.scan.context))
+                                    val adapter = ArrayAdapter(dialogBinding.scan.context, android.R.layout.simple_list_item_1, resources.getStringArray(R.array.product_units))
+                                    quantityDialogBinding.unitView.setAdapter(adapter)
+                                    quantityDialog.setView(quantityDialogBinding.root)
+                                    quantityDialog.setPositiveButton("Ok", null)
+                                    quantityDialog.setNegativeButton("Cancel", null);
+                                    quantityDialog.show()
+                                } else{
+                                    Toast.makeText(dialogBinding.scan.context, "No product was found in database.", Toast.LENGTH_SHORT)
+                                }
+                            } else {
+                                Toast.makeText(dialogBinding.scan.context, "Something went wrong while making a request.", Toast.LENGTH_SHORT)
+                            }
                         }
                     })
                 }
--- app/src/main/java/com/example/befit/services/FoodApiResponse.kt
+++ app/src/main/java/com/example/befit/services/FoodApiResponse.kt
@@ -4,20 +4,22 @@ import com.squareup.moshi.Json
 
 data class FoodApiResponse(
     @Json(name="code")
-    val Code: String?,
+    val Code: String,
     @Json(name="product")
     val Product: Product?,
     @Json(name="status")
-    val Status: Long?
+    val Status: Int
 )
 
 data class Product(
-    @Json(name="nutriments") val Nutriments: Nutriments
+    @Json(name="nutriments") val Nutriments: Nutriments,
+    @Json(name="product_name") val ProductName: String
+
 )
 
 data class Nutriments(
     @Json(name="carbohydrates_100g") val Carbohydrates100g: Double,
-    @Json(name="energy-kcal_100g") val EnergyKcal100g: Long,
+    @Json(name="energy-kcal_100g") val EnergyKcal100g: Double,
     @Json(name="fat_100g") val Fat100g: Double,
     @Json(name="proteins_100g") val Proteins100g: Double
 )
